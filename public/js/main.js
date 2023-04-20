3234796a1690c48ec92004499f60b12778ee28e7
LebCit <lebcit@gmail.com>
2023-04-20 10:17:38 +0300

    Replace gg-external class by icon

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,12 @@ allLinks.forEach((link) => {
 		link.setAttribute("rel", "external noopener noreferrer")
 		link.insertAdjacentHTML(
 			"beforeend",
-			'<i class="gg-external" role ="img" aria-label=" (opens in a new tab)"></i>'
+			`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: bottom;">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+<path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path>
+<path d="M11 13l9 -9"></path>
+<path d="M15 4h5v5"></path>
+</svg>`
 		)
 	}
 })
