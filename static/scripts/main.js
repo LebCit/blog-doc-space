ddd1aaba6668dd730f0eb22068a2e6292d5c07e6
LebCit <lebcit@gmail.com>
2023-11-24 09:55:01 +0200

    Update and move main.js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,6 @@
+console.log("%cBlog-Doc for Deta Space", "font-weight:bold; font-size:48px;")
+console.log("The simplest Node.js CMS and SSG! → https://github.com/LebCit/blog-doc-space")
+
 if (!window.location.pathname.startsWith("/admin")) {
 	// Toggle sidebar menu
 	let elements = document.querySelectorAll("html, #menu, #layout, .footer")
@@ -34,6 +37,7 @@ allLinks.forEach((link) => {
 		link.insertAdjacentHTML(
 			"beforeend",
 			`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: bottom;">
+<title>External link</title>
 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path>
 <path d="M11 13l9 -9"></path>
