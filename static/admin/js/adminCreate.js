12c4db81700b204e37443b10916dc8b1e9bb089f
LebCit <lebcit@gmail.com>
2023-11-24 09:14:23 +0200

    Format file title, check if file exists

--- static/admin/js/adminCreate.js
+++ static/admin/js/adminCreate.js
@@ -52,17 +52,17 @@ For example, if you give your page the following title :
 \`My awesome page title\`
 The URL to the page will be \`https://domain.name/pages/my-awesome-page-title\`
 
-So choose wisely your page's title the first time you create it !
-⚠️ You'll be able to update the page's title but this will not change the page's URL !
+⚠️ You'll not be able to update the page's title!
+So choose wisely your page's title the first time you create it!
 
 ## Optional field
 
 The \`image\` field is optional.
-It means that you can leave it empty, it's totally up to you !
+It means that you can leave it empty, it's totally up to you!
 
 ## The content
 
-Now remove all of this page's content, and start writing your next awesome page !
+Now remove all of this page's content, and start writing your next awesome page!
 Use Markdown to write and format the markup of your page,
 click on the preview tab to have an idea of the content's output.
 Finally, click the submit button to create your new page 😉`)
@@ -92,23 +92,52 @@ Finally, click the submit button to create your new page 😉`)
 					})
 				})
 			} else {
-				Swal.fire({
-					title: "Create a page ?!",
-					html: `By clicking on <b>Create</b>,<br>a new <b>page</b> will be created,<br>with the content provided in the editor.`,
-					icon: "question",
-					showCancelButton: true,
-					confirmButtonText: "Create",
-					didOpen: () => {
-						const mdContent = editor.getMarkdown()
-						const textArea = document.getElementById("file-contents")
-						textArea.value += mdContent
-						const b = Swal.getConfirmButton()
-						b.type = "button"
-						b.addEventListener("click", () => {
-							fileForm.submit()
-						})
-					},
-				})
+				const pageTitleValue = document.getElementById("page-title-input").value
+				const pageTitle = pageTitleValue
+					.toLowerCase()
+					.replace(/[^a-zA-Z0-9-_ ]/g, "") // Remove special characters except hyphen and underscore
+					.replace(/_+/g, "-") // Replace any number of underscore by one hyphen
+					.replace(/\s+/g, "-") // Replace any number of space by one hyphen
+					.replace(/^-+/, "") // Remove any number of hyphen at the beginning
+					.replace(/-+/g, "-") // Replace any number of hyphen by one hyphen only
+					.replace(/-+$/, "") // Remove any number of hyphen at the end
+
+				fetch("/check-admin-create")
+					.then((res) => res.json())
+					.then((data) => {
+						const filename = data.find((title) => title === `${pageTitle}.md`)
+						// If no file with the same name is found create it!
+						if (filename === undefined) {
+							Swal.fire({
+								title: "Create a page?!",
+								html: `By clicking on <b>Create</b>,<br>a new <b>page</b> will be created,<br>with the content provided in the editor.`,
+								icon: "question",
+								showCancelButton: true,
+								confirmButtonText: "Create",
+								didOpen: () => {
+									const mdContent = editor.getMarkdown()
+									const textArea = document.getElementById("file-contents")
+									textArea.value += mdContent
+									const b = Swal.getConfirmButton()
+									b.type = "button"
+									b.addEventListener("click", () => {
+										fileForm.submit()
+									})
+								},
+							})
+						} else {
+							Swal.fire({
+								title: "This file already exists!",
+								html: `A file with the same title has been found!<br>Change the title to create a new page!<br>Remember that you can update a page!`,
+								icon: "warning",
+								showConfirmButton: false,
+								showCancelButton: true,
+							})
+						}
+					})
+					.catch((err) => {
+						console.error("Error: ", err)
+					})
 			}
 		})
 	} else if (fileTypeSelectValue === "post") {
@@ -136,17 +165,17 @@ For example, if you give your post the following title :
 \`My awesome post title\`
 The URL to the post will be \`https://domain.name/posts/my-awesome-post-title\`
 
-So choose wisely your post's title the first time you create it !
-⚠️ You'll be able to update the post's title but this will not change the post's URL !
+⚠️ You'll not be able to update the post's title!
+So choose wisely your post's title the first time you create it!
 
 ## Optional fields
 
 The \`description\`, \`image\` and \`tag(s)\` fields are optional.
-It means that you can leave any of them or all of them empty, it's totally up to you !
+It means that you can leave any of them or all of them empty, it's totally up to you!
 
 ## The content
 
-Now remove all of this post's content, and start writing your next awesome post !
+Now remove all of this post's content, and start writing your next awesome post!
 Use Markdown to write and format the markup of your post,
 click on the preview tab to have an idea of the content's output.
 Finally, click the submit button to create your new page 😉`)
@@ -176,23 +205,52 @@ Finally, click the submit button to create your new page 😉`)
 					})
 				})
 			} else {
-				Swal.fire({
-					title: "Create a post ?!",
-					html: `By clicking on <b>Create</b>,<br>a new <b>post</b> will be created,<br>with the content provided in the editor.`,
-					icon: "question",
-					showCancelButton: true,
-					confirmButtonText: "Create",
-					didOpen: () => {
-						const mdContent = editor.getMarkdown()
-						const textArea = document.getElementById("file-contents")
-						textArea.value += mdContent
-						const b = Swal.getConfirmButton()
-						b.type = "button"
-						b.addEventListener("click", () => {
-							fileForm.submit()
-						})
-					},
-				})
+				const postTitleValue = document.getElementById("post-title-input").value
+				const postTitle = postTitleValue
+					.toLowerCase()
+					.replace(/[^a-zA-Z0-9-_ ]/g, "") // Remove special characters except hyphen and underscore
+					.replace(/_+/g, "-") // Replace any number of underscore by one hyphen
+					.replace(/\s+/g, "-") // Replace any number of space by one hyphen
+					.replace(/^-+/, "") // Remove any number of hyphen at the beginning
+					.replace(/-+/g, "-") // Replace any number of hyphen by one hyphen only
+					.replace(/-+$/, "") // Remove any number of hyphen at the end
+
+				fetch("/check-admin-create")
+					.then((res) => res.json())
+					.then((data) => {
+						const filename = data.find((title) => title === `${postTitle}.md`)
+						// If no file with the same name is found create it
+						if (filename === undefined) {
+							Swal.fire({
+								title: "Create a post?!",
+								html: `By clicking on <b>Create</b>,<br>a new <b>post</b> will be created,<br>with the content provided in the editor.`,
+								icon: "question",
+								showCancelButton: true,
+								confirmButtonText: "Create",
+								didOpen: () => {
+									const mdContent = editor.getMarkdown()
+									const textArea = document.getElementById("file-contents")
+									textArea.value += mdContent
+									const b = Swal.getConfirmButton()
+									b.type = "button"
+									b.addEventListener("click", () => {
+										fileForm.submit()
+									})
+								},
+							})
+						} else {
+							Swal.fire({
+								title: "This file already exists!",
+								html: `A file with the same title has been found!<br>Change the title to create a new post!<br>Remember that you can update a post!`,
+								icon: "warning",
+								showConfirmButton: false,
+								showCancelButton: true,
+							})
+						}
+					})
+					.catch((err) => {
+						console.error("Error: ", err)
+					})
 			}
 		})
 	} else {
